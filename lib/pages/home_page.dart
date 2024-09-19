4e62e0cd73956e122cfdc372bf81d305b84c4bf1
damooola <damolanelson@gmail.com>
2024-09-19 18:14:02 +0100

    use listview builder instead for cleaner code

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,9 +9,21 @@ import 'chat_page.dart';
 class HomePage extends StatelessWidget {
   HomePage({super.key});
 
-  //get chat and auth servyies
+  // initialise chat and auth servyies
   final AuthService authService = AuthService();
   final ChatService chatService = ChatService();
+
+  void tapUserTile(BuildContext context, Map<String, dynamic> userData) {
+    Navigator.push(
+        context,
+        //go to chat page
+        MaterialPageRoute(
+          builder: (context) => ChatPage(
+            receiverEmail: userData["email"],
+            receiverID: userData["uid"],
+          ),
+        ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +38,9 @@ class HomePage extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      drawer: const MyDrawer(),
+      drawer: MyDrawer(
+        currentUserEmail: authService.getCurrentUser()!.email!,
+      ),
       body: _buildUserList(),
     );
   }
@@ -44,34 +58,32 @@ class HomePage extends StatelessWidget {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: Text("Loading..."));
           }
-          //return list view
-          return ListView(
+          // use a listview builder instead
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              final userData = snapshot.data![index];
+              return _userListItem(userData, context);
+            },
+          );
+          //alternative way of displaying the list of users by using a listview
+          /* return ListView(
             children: snapshot.data!
                 .map<Widget>(
-                    (userData) => _buildUserListItem(userData, context))
-                .toList(),
-          );
+                     (userData) => _buildUserListItem(userData, context))
+                 .toList(),
+           ); */
         });
   }
 
-// individual list tile for users
-  Widget _buildUserListItem(
-      Map<String, dynamic> userData, BuildContext context) {
+// individual user tile for users
+  Widget _userListItem(Map<String, dynamic> userData, BuildContext context) {
     // display all users except current user
     if (userData["email"] != authService.getCurrentUser()!.email) {
       return MyUserTile(
         text: userData["email"],
-        onTap: () {
-          //go to chat page
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => ChatPage(
-                  receiverEmail: userData["email"],
-                  receiverID: userData["uid"],
-                ),
-              ));
-        },
+        //tap user tile
+        onTap: () => tapUserTile(context, userData),
       );
     } else {
       return Container();
